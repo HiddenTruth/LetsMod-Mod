002a44d7503876fb729f1f841c3de8ad5ebb7e01
HiddenTruth <computerman45@gmail.com>
2013-04-20 14:48:00 -0500

    Finished watching LetsMod 5 from Pahimar.

---
+++ common/com/hiddentruth2012/letsmod/LetsMod.java
@@ -0,0 +1,31 @@
+package com.hiddentruth2012.letsmod;
+
+import com.hiddentruth2012.lib.Reference;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class LetsMod {
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event){
+        
+    }
+    
+    @Init
+    public void init(FMLInitializationEvent event){
+        
+    }
+    
+    @PostInit
+    public void postInit(FMLPostInitializationEvent event){
+        
+    }
+
+}
